640a86972386a86c0673c40111b51bcdfb04990a
Alessitom <alexmolerotorres@gmail.com>
2024-04-12 23:48:38 +0200

    Añadir y Lista de Productos

--- src/Tienda_Package/Almacen.java
+++ src/Tienda_Package/Almacen.java
@@ -1,5 +1,44 @@
 package Tienda_Package;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Almacen {
+
+	Map<String, Integer> Mapa;
+
+	public Almacen() {
+	 Mapa = new HashMap<>();
+
+	}
+
+	public void Añadirproducto(String nombre, Integer stock) {
+		if (Mapa.containsKey(nombre)) {
+			System.out.println("Este producto ya está en el Almacén");
+		} else {
+			Mapa.put(nombre, stock);
+		}
+	}
+
+	public void ListaProductos() {
+	    StringBuilder Lista = new StringBuilder();
+	    Lista.append("   Nombre               Stock\n");
+	    Lista.append("============         ===========\n");
+
+	    for (String nombre : Mapa.keySet()) {
+	        Integer stock = Mapa.get(nombre);
+	    Lista.append(String.format("%5s\t\t\t%d\n", nombre, stock));
+	    }	
+	    System.out.println(Lista.toString());
+	}
+
+
+	public Map<String, Integer> getMapa() {
+		return Mapa;
+	}
+
+	public void setMapa(Map<String, Integer> mapa) {
+		Mapa = mapa;
+	}
 
 }
--- src/Tienda_Package/App.java
+++ src/Tienda_Package/App.java
@@ -1,10 +1,54 @@
 package Tienda_Package;
 
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);	
+		Almacen almacen = new Almacen();
+		boolean salir = false;
+		do {
+			Menu();
+			int opcion = scanner.nextInt();
+			switch (opcion) {
+			case 1:
+				almacen.ListaProductos();
+				break;
+			 case 2:
+                 System.out.println("Introduce el nombre del producto:");
+                 scanner.nextLine(); // Consumir el salto de línea pendiente
+                 String nombreProducto = scanner.nextLine();
+                 System.out.println("Introduce el stock: ");
+                 Integer stock = scanner.nextInt();
+                 almacen.Añadirproducto(nombreProducto, stock);
+                 break;
+			case 7: 
+				System.out.println("Has salido con éxito");
+				 salir = true;
+				 break;
+			default:
+				
+			}
+		} while (!salir);
 		
-
+	}
+	
+	
+	
+	
+	public static void Menu() {
+		System.out.println("********************------------MENÚ--------------**************************");
+		System.out.println("1. Ver Inventario ");
+		System.out.println("2. Añadir producto");
+		System.out.println("3. Retirar producto");
+		System.out.println("4. Ordenar Almacén (Por Orden Alfabético)");
+		System.out.println("5. Productos que están sin stock");
+		System.out.println("6. Reponer stock (Producto y Cantidad)");
+		System.out.println("7. Salir ");
+		System.out.println("********************------------MENÚ--------------**************************");
+		
+		
 	}
 
 }
